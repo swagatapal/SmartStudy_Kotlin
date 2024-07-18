3f30620993b4eef433dbc7063e519add317564ff
swagatapal <swagatapal1980@gmail.com>
2024-07-18 23:19:53 +0530

    Task screen

--- app/src/main/java/com/example/studysmart/MainActivity.kt
+++ app/src/main/java/com/example/studysmart/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.studysmart.domain.model.Subject
 import com.example.studysmart.domain.model.Task
 import com.example.studysmart.presentation.dashboard.DashboardScreen
 import com.example.studysmart.presentation.subject.SubjectScreen
+import com.example.studysmart.presentation.task.TaskScreen
 import com.example.studysmart.presentation.theme.StudySmartTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +17,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             StudySmartTheme {
                 //DashboardScreen()
-                SubjectScreen()
+                //SubjectScreen()
+                TaskScreen()
             }
         }
     }
--- app/src/main/java/com/example/studysmart/presentation/task/TaskScreen.kt
+++ app/src/main/java/com/example/studysmart/presentation/task/TaskScreen.kt
@@ -1,27 +1,57 @@
 package com.example.studysmart.presentation.task
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.studysmart.presentation.components.TaskCheckBox
 import com.example.studysmart.presentation.theme.Red
+import com.example.studysmart.util.Priority
 
 
 @Composable
 fun TaskScreen() {
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+    
+    var taskTitleError by rememberSaveable { mutableStateOf<String?>(null) }
+    taskTitleError = when{
+        title.isBlank()->"Please enter a task title."
+        title.length<4 ->"Task title is too short."
+        title.length>30 ->"Task title is too long."
+        else -> null
+    }
     Scaffold(
         topBar = {
             TaskScreenTopBar(
@@ -35,10 +65,66 @@ fun TaskScreen() {
 
         }
     ) {paddingValue->
-        Column(modifier = Modifier.fillMaxSize()
+        Column(modifier = Modifier
+            .fillMaxSize()
             .padding(paddingValue)
-            .padding(horizontal = 12.dp)){
+            .padding(horizontal = 12.dp)
+        ){
+            OutlinedTextField(
+                modifier =Modifier.fillMaxWidth() ,
+                value = title,
+                onValueChange ={title = it},
+                label = { Text(text = "Title") },
+                singleLine = true,
+                isError = taskTitleError !=null && title.isNotBlank(),
+                supportingText = { Text(text = taskTitleError.orEmpty())}
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedTextField(
+                modifier =Modifier.fillMaxWidth() ,
+                value = description,
+                onValueChange ={description = it},
+                label = { Text(text = "Description") },
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(text = "Due Date", style = MaterialTheme.typography.bodySmall)
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically)
+            {
+                Text(text = "English", style = MaterialTheme.typography.bodyLarge)
+                IconButton(
+                    onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.DateRange,
+                        contentDescription ="Select due date" )
 
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Priority", style = MaterialTheme.typography.bodySmall)
+            Spacer(modifier = Modifier.height(10.dp))
+            Row (modifier = Modifier.fillMaxWidth()){
+                Priority.entries.forEach{ priority->
+                    PriorityButton(
+                        modifier = Modifier.weight(1f),
+                        label = priority.title,
+                        backgroundColor = priority.color,
+                        borderColor = if(priority == Priority.Medium){
+                            Color.White
+                        }else Color.Transparent,
+                        labelColor = if(priority==Priority.Medium)
+                        {
+                            Color.White
+                        }else Color.White.copy(alpha = 0.7f),
+                        onClick = {}
+
+
+                    )
+                }
+
+            }
         }
         
     }
@@ -66,7 +152,9 @@ private fun TaskScreenTopBar(
 
             }
         },
-        title = { Text(text ="Task" ) },
+        title = { Text(
+            text ="Task" ,
+            style = MaterialTheme.typography.headlineSmall) },
         actions = {
             if(isTaskExits)
             {
@@ -86,4 +174,28 @@ private fun TaskScreenTopBar(
         }
     )
     
+}
+
+@Composable
+private fun PriorityButton(
+    modifier: Modifier = Modifier,
+    label: String,
+    backgroundColor:Color,
+    borderColor: Color,
+    labelColor:Color,
+    onClick:()->Unit
+){
+    Box(
+        modifier = modifier
+            .background(backgroundColor)
+            .clickable { onClick() }
+            .padding(5.dp)
+            .border(1.dp, borderColor, RoundedCornerShape(5.dp))
+            .padding(5.dp),
+        contentAlignment = Alignment.Center
+
+    ){
+        Text(text = label, color = labelColor)
+    }
+
 }
